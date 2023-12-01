7118305d62f03529b8d63aeb308d50e2dab97b06
kasra-haghpanah <kasrakhpk1985@gmail.com>
2023-12-01 21:15:05 +0330

    first commit

--- slave/src/main/java/com/example/batchprocessing/slave/SlaveApplication.java
+++ slave/src/main/java/com/example/batchprocessing/slave/SlaveApplication.java
@@ -2,9 +2,8 @@ package com.example.batchprocessing.slave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SlaveApplication {
 
     public static void main(String[] args) {
