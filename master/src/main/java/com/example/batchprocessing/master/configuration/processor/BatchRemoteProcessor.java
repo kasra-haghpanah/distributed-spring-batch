4eb4863cd1906658d2981d24d8a40f2992a560c5
kasra-haghpanah <kasrakhpk1985@gmail.com>
2023-12-16 02:13:08 +0330

    first commit

--- master/src/main/java/com/example/batchprocessing/master/configuration/processor/BatchRemoteProcessor.java
+++ master/src/main/java/com/example/batchprocessing/master/configuration/processor/BatchRemoteProcessor.java
@@ -2,8 +2,6 @@ package com.example.batchprocessing.master.configuration.processor;
 
 import com.example.batchprocessing.master.configuration.structure.Customer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.batch.integration.async.AsyncItemProcessor;
-import org.springframework.batch.integration.async.AsyncItemWriter;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -11,10 +9,8 @@ import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class BatchRemoteProcessor {
@@ -33,18 +29,6 @@ public class BatchRemoteProcessor {
         };
     }
 
-/*    @Bean
-    @Qualifier("masterAsyncItemProcessor")
-    public AsyncItemProcessor itemProcessor(
-            @Qualifier("masterItemProcessor") ItemProcessor<List<Customer>, String> itemProcessor,
-            TaskExecutor taskExecutor
-    ) {
-        AsyncItemProcessor asyncItemProcessor = new AsyncItemProcessor();
-        asyncItemProcessor.setTaskExecutor(taskExecutor);
-        asyncItemProcessor.setDelegate(itemProcessor);
-        return asyncItemProcessor;
-    }*/
-
     @Bean
     @Qualifier("masterItemWriter")
     ItemWriter<Object> masterItemWriter() {
@@ -52,13 +36,5 @@ public class BatchRemoteProcessor {
             System.out.println(item);
         };
     }
-
-/*    @Bean
-    @Qualifier("masterAsyncItemWriter")
-    public AsyncItemWriter writer(@Qualifier("masterItemWriter") ItemWriter<String> masterItemWriter) {
-        AsyncItemWriter asyncItemWriter = new AsyncItemWriter();
-        asyncItemWriter.setDelegate(masterItemWriter);
-        return asyncItemWriter;
-    }*/
 
 }
