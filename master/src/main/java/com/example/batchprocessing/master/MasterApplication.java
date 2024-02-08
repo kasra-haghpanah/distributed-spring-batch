5628714472e3277fc11876e53eae8fa4bb0b233a
kasra-haghpanah <kasrakhpk1985@gmail.com>
2024-02-09 00:27:52 +0330

    The Spring Retry has been added for when the database or message broker is disconnected to be connected again.

--- master/src/main/java/com/example/batchprocessing/master/MasterApplication.java
+++ master/src/main/java/com/example/batchprocessing/master/MasterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasterApplication {
-
+    // java -DspringAot=true -jar master.jar
     public static void main(String[] args) {
         System.setProperty("spring.amqp.deserialization.trust.all","true");
         SpringApplication.run(MasterApplication.class, args);
